2ecbd99fc8b7f9c7bb12971729d3596ee4b7035b
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2023-10-09 16:21:08 +0000

    unordered map question only

---
+++ DSA/striverSheet/Day2/unordered_map.cpp
@@ -0,0 +1,306 @@
+//----------------------------------------------- Question 1 -----------------------------------------
+
+
+
+// Given an array which may contain duplicates, print all elements and their frequencies.
+
+// Examples: 
+
+// Input :  arr[] = {10, 20, 20, 10, 10, 20, 5, 20}
+// Output : 10 3
+//          20 4
+//          5  1
+
+// Input : arr[] = {10, 20, 20}
+// Output : 10 1
+//          20 2 
+
+
+
+//-------------------------------------------question 2-------------------------------------------------
+
+// Given an unsorted array of integers, find the number of subarrays having a sum exactly equal to a given number k.
+
+// Examples: 
+
+// Input : arr[] = {10, 2, -2, -20, 10}, k = -10
+// Output : 3
+// Explanation: Subarrays: arr[0…3], arr[1…4], arr[3..4] have a sum exactly equal to -10.
+
+// Input : arr[] = {9, 4, 20, 3, 10, 5}, k = 33
+// Output : 2
+// Explanation: Subarrays : arr[0…2], arr[2…4] have a sum exactly equal to 33.
+
+
+
+
+
+
+
+//------------------------------------------------Question 3------------------------------------------------------------
+// Given an unsorted array of integers, find the number of subarrays having a sum exactly equal to a given number k.
+
+// Examples: 
+
+// Input : arr[] = {10, 2, -2, -20, 10}, k = -10
+// Output : 3
+// Explanation: Subarrays: arr[0…3], arr[1…4], arr[3..4] have a sum exactly equal to -10.
+
+// Input : arr[] = {9, 4, 20, 3, 10, 5}, k = 33
+// Output : 2
+// Explanation: Subarrays : arr[0…2], arr[2…4] have a sum exactly equal to 33.
+
+
+
+
+//------------------------------------------------Question 4------------------------------------------------------------
+
+// Given an array of items, an i-th index element denotes the item id’s, and given a number m, the task is to remove m elements such that there should be minimum distinct id’s left. Print the number of distinct id’s.
+
+// Examples: 
+
+// Input : arr[] = { 2, 2, 1, 3, 3, 3} 
+//             m = 3
+// Output : 1
+// Remove 1 and both 2's.So, only 3 will be 
+// left that's why distinct id is 1.
+// Input : arr[] = { 2, 4, 1, 5, 3, 5, 1, 3} 
+//             m = 2
+// Output : 3
+// Remove 2 and 4 completely. So, remaining ids 
+// are 1, 3 and 5 i.e. 3
+
+
+
+//---------------------------------------------------------------- Question 5-----------------------------------------
+
+// Given an array of items, an i-th index element denotes the item id’s, and given a number m, the task is to remove m elements such that there should be minimum distinct id’s left. Print the number of distinct id’s.
+
+// Examples: 
+
+// Input : arr[] = { 2, 2, 1, 3, 3, 3} 
+//             m = 3
+// Output : 1
+// Remove 1 and both 2's.So, only 3 will be 
+// left that's why distinct id is 1.
+// Input : arr[] = { 2, 4, 1, 5, 3, 5, 1, 3} 
+//             m = 2
+// Output : 3
+// Remove 2 and 4 completely. So, remaining ids 
+// are 1, 3 and 5 i.e. 3
+
+
+
+
+//---------------------------------------------------------------- Question 6-----------------------------------------
+
+// Given string str, the task is to find the minimum count of characters that need to be deleted from the string such that the frequency of each character of the string is unique.
+
+// Examples:
+
+// Input: str = “ceabaacb” 
+// Output: 2 
+// Explanation: 
+// The frequencies of each distinct character are as follows: 
+// c —> 2 
+// e —> 1 
+// a —> 3 
+// b —> 2 
+// Possible ways to make frequency of each character unique by minimum number of moves are: 
+
+// Removing both occurrences of ‘c’ modifies str to “eabaab”
+// Removing an occurrence of ‘c’ and ‘e’ modifies str to “abaacb”
+// Therefore, the minimum removals required is 2.
+
+
+
+
+
+
+
+
+//---------------------------------------------------------------- Question 7-----------------------------------------
+
+// Given an unsorted array of integers, print the array after removing the duplicate elements from it. We need to print distinct array elements according to their first occurrence.
+
+// Examples: 
+
+// Input : arr[] = { 1, 2, 5, 1, 7, 2, 4, 2}
+// Output : 1 2 5 7 4
+// Explanation : {1, 2} appear more than one time.
+
+
+
+
+
+//---------------------------------------------------------------- Question 8-----------------------------------------
+
+// Given an unsorted array of integers, print the array after removing the duplicate elements from it. We need to print distinct array elements according to their first occurrence.
+
+// Examples: 
+
+// Input : arr[] = { 1, 2, 5, 1, 7, 2, 4, 2}
+// Output : 1 2 5 7 4
+// Explanation : {1, 2} appear more than one time.
+
+
+
+
+
+
+//---------------------------------------------------------------- Question 9-----------------------------------------
+
+// Given a string, convert the string to palindrome without any modifications like adding a character, removing a character, replacing a character etc. 
+
+// Examples: 
+
+// Input : "mdaam"
+// Output : "madam" or "amdma"
+
+// Input : "abb"
+// Output : "bab"
+
+// Input : "geeksforgeeks"
+// Output : "No Palindrome"
+
+
+
+
+
+
+
+//
+//---------------------------------------------------------------- Question 10-----------------------------------------
+// Given an array with n positive integers. We need to find the minimum number of operations to make all elements equal. We can perform addition, multiplication, subtraction, or division with any part on an array element. 
+
+// Examples: 
+
+// Input : arr[] = {1, 2, 3, 4}
+// Output : 3
+// Since all elements are different, 
+// we need to perform at least three
+// operations to make them same. For
+// example, we can make them all 1
+// by doing three subtractions. Or make
+// them all 3 by doing three additions.
+
+// Input : arr[] = {1, 1, 1, 1}
+// Output : 0
+
+
+
+
+//---------------------------------------------------------------- Question 11-----------------------------------------
+
+// Given an array with n positive integers. We need to find the minimum number of operations to make all elements equal. We can perform addition, multiplication, subtraction, or division with any part on an array element. 
+
+// Examples: 
+
+// Input : arr[] = {1, 2, 3, 4}
+// Output : 3
+// Since all elements are different, 
+// we need to perform at least three
+// operations to make them same. For
+// example, we can make them all 1
+// by doing three subtractions. Or make
+// them all 3 by doing three additions.
+
+// Input : arr[] = {1, 1, 1, 1}
+// Output : 0
+
+
+//---------------------------------------------------------------- Question 12-----------------------------------------
+// Given an array with n positive integers. We need to find the minimum number of operations to make all elements equal. We can perform addition, multiplication, subtraction, or division with any part on an array element. 
+
+// Examples: 
+
+// Input : arr[] = {1, 2, 3, 4}
+// Output : 3
+// Since all elements are different, 
+// we need to perform at least three
+// operations to make them same. For
+// example, we can make them all 1
+// by doing three subtractions. Or make
+// them all 3 by doing three additions.
+
+// Input : arr[] = {1, 1, 1, 1}
+// Output : 0
+
+
+////---------------------------------------------------------------- Question 13-----------------------------------------
+// Given an array with n positive integers. We need to find the minimum number of operations to make all elements equal. We can perform addition, multiplication, subtraction, or division with any part on an array element. 
+
+// Examples: 
+
+// Input : arr[] = {1, 2, 3, 4}
+// Output : 3
+// Since all elements are different, 
+// we need to perform at least three
+// operations to make them same. For
+// example, we can make them all 1
+// by doing three subtractions. Or make
+// them all 3 by doing three additions.
+
+// Input : arr[] = {1, 1, 1, 1}
+// Output : 0
+
+
+//---------------------------------------------------------------- Question 14-----------------------------------------
+// Given an array with n positive integers. We need to find the minimum number of operations to make all elements equal. We can perform addition, multiplication, subtraction, or division with any part on an array element. 
+
+// Examples: 
+
+// Input : arr[] = {1, 2, 3, 4}
+// Output : 3
+// Since all elements are different, 
+// we need to perform at least three
+// operations to make them same. For
+// example, we can make them all 1
+// by doing three subtractions. Or make
+// them all 3 by doing three additions.
+
+// Input : arr[] = {1, 1, 1, 1}
+// Output : 0
+
+
+//---------------------------------------------------------------- Question 15-----------------------------------------
+// Given an array with n positive integers. We need to find the minimum number of operations to make all elements equal. We can perform addition, multiplication, subtraction, or division with any part on an array element. 
+
+// Examples: 
+
+// Input : arr[] = {1, 2, 3, 4}
+// Output : 3
+// Since all elements are different, 
+// we need to perform at least three
+// operations to make them same. For
+// example, we can make them all 1
+// by doing three subtractions. Or make
+// them all 3 by doing three additions.
+
+// Input : arr[] = {1, 1, 1, 1}
+// Output : 0
+
+
+//---------------------------------------------------------------- Question 16-----------------------------------------
+
+
+
+
+//---------------------------------------------------------------- Question 17-----------------------------------------
+
+
+
+//---------------------------------------------------------------- Question 18-----------------------------------------
+
+
+
+
+
+
+//---------------------------------------------------------------- Question 19-----------------------------------------
+
+
+
+
+
+//---------------------------------------------------------------- Question 20-----------------------------------------

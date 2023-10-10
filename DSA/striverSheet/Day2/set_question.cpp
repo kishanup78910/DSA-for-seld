f8c74e3d8c7e34559c330f8ae14b3f76daf89611
Kishan Upadhyay <79304976+kishanup78910@users.noreply.github.com>
2023-10-10 12:15:29 +0000

    STl queestion done

---
+++ DSA/striverSheet/Day2/set_question.cpp
@@ -0,0 +1,67 @@
+// Example 1: Set Operations
+// Problem Statement:
+// You are given two sets, Set A and Set B, both containing integers. Implement a C++ program to perform the following set operations:
+
+// Union: Compute and print the union of Set A and Set B.
+// Intersection: Compute and print the intersection of Set A and Set B.
+// Difference: Compute and print the elements present in Set A but not in Set B.
+// Symmetric Difference: Compute and print the symmetric difference of Set A and Set B.
+// Example 2: Finding Duplicates
+// Problem Statement:
+// You are given an array of integers. Write a C++ program to find and print all the duplicate elements in the array using a set data structure.
+
+// Example 3: Frequency Count using Set
+// Problem Statement:
+// You are given an array of integers. Write a C++ program to count the frequency of each element in the array using a set.
+
+// Example 4: Set of Strings
+// Problem Statement:
+// You are given a list of strings. Write a C++ program to store these strings in a set and then perform operations like finding the number of unique strings and finding the lexicographically smallest and largest string in the set.
+
+// Example 5: Set and Sorting
+// Problem Statement:
+// You are given a list of integers. Write a C++ program to store these integers in a set and then print them in ascending order.
+
+// Example 6: Set and Binary Search
+// Problem Statement:
+// You are given a sorted array of integers. Write a C++ program to convert the array into a set and then perform binary search operations to find if a given element is present in the set or not.
+
+
+
+
+
+
+
+
+
+// Example 7: Set and Prime Numbers
+// Problem Statement:
+// Write a C++ program to generate and store all prime numbers up to a given limit 'N' in a set. Implement a function to check if a given number is a prime number or not. Then, print the prime numbers stored in the set.
+
+// Example 8: Set and Anagrams
+// Problem Statement:
+// You are given a list of strings. Write a C++ program to group anagrams from the list. Two strings are anagrams if they have the same characters but in a different order. Store these groups of anagrams in sets and print them.
+
+// Example 9: Set and Subsequences
+// Problem Statement:
+// You are given a string. Write a C++ program to generate all possible subsequences of the string and store them in a set. Subsequences are the strings that can be formed by removing some characters (or none) without changing the order of the remaining characters. Print the unique subsequences stored in the set.
+
+// Example 10: Set and Pair Sum
+// Problem Statement:
+// You are given an array of integers and a target sum. Write a C++ program to find all pairs of integers in the array that add up to the target sum. Store these pairs in a set and print them. Ensure that each pair is stored only once, regardless of the order.
+
+// Example 11: Set and Palindromes
+// Problem Statement:
+// Write a C++ program to read a list of strings. Store all palindromic strings (strings that read the same backward as forward) in a set. After reading all strings, print the unique palindromes stored in the set.
+
+// Example 12: Set and Powerset
+// Problem Statement:
+// You are given a set of integers. Write a C++ program to generate all subsets (including the empty set) of the given set and store them in a set of sets. Finally, print the subsets stored in the set.
+
+// Example 13: Set and Largest Subset
+// Problem Statement:
+// You are given an array of integers. Write a C++ program to find the largest subset of the array such that no two elements in the subset have a common factor other than 1. Store the elements of this subset in a set and print them.
+
+// Example 14: Set and Matrix Operations
+// Problem Statement:
+// You are given two matrices represented as sets of sets (sets of integers). Write a C++ program to perform matrix addition and multiplication operations using sets. Print the resulting matrices after each operation.
